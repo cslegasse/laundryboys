33c99be7f582338ad8849303e36bb94c9f5eea41
cslegasse <legasseahs1@gmail.com>
2025-09-07 13:49:06 -0400

    why

--- backend/app/test_main.py
+++ backend/app/test_main.py
@@ -1,11 +1,14 @@
 from fastapi.testclient import TestClient
-from .main import app
-
+from app.main import app 
 
 client = TestClient(app)
-
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+def test_users():
+    response = client.get("/users/")
+    assert response.status_code == 200
+    assert response.json() == {"users": []}
