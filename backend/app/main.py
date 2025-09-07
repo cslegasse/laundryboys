285b0d482c6ad85523c2bf531f78b3c0b71bdfdf
cslegasse <legasseahs1@gmail.com>
2025-09-07 13:35:48 -0400

    backend ci

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,13 @@
-# backend/app/main.py
 from fastapi import FastAPI
 from .api import users
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
 
 app.include_router(users.router, prefix="/users", tags=["users"])
